6515d99d5c020c4e428b3dbea95d882fe61a8548
Preston Lamb <preston.j.lamb@gmail.com>
2019-05-25 23:07:59 -0600

    set up travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -28,5 +28,12 @@ module.exports = function(config) {
 		browsers: ['Chrome'],
 		singleRun: false,
 		restartOnFileChange: true,
+		browsers: ['Chrome'],
+		customLaunchers: {
+			ChromeHeadlessNoSandbox: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox'],
+			},
+		},
 	});
 };
