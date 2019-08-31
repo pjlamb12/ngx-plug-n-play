a7a47fb3a964111e42d2f977f99d5e3d0b9e7b18
Preston Lamb <preston.j.lamb@gmail.com>
2019-08-30 23:28:06 -0600

    added browser animations module for demo site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
 	AlertToasterModule,
 	TypeaheadModule,
@@ -19,6 +20,7 @@ import { AccordionDemoComponent } from './accordion-demo/accordion-demo.componen
 @NgModule({
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		FormsModule,
 		AppRoutingModule,
 		TypeaheadModule,
